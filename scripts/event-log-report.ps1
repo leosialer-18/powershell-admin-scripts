a3126f1320c5c6e80df72ed0c49c510b0df7c9a6
Leonardo Sialer Gonzales <231833766+leosialer-18@users.noreply.github.com>
2026-07-25 17:37:42 +0200

    Add Windows event log reporting script

---
+++ scripts/event-log-report.ps1
@@ -0,0 +1,17 @@
+Write-Host "==========================="
+Write-Host "WINDOWS EVENT LOG REPORT"
+Write-Host "==========================="
+Write-Host ""
+
+$events = Get-WinEvent -Logname System -MaxEvents 10 | Where-Object {$_.LevelDisplayName -eq "Advertencia" -or $_.LevelDisplayName -eq "Error"}
+
+$events | ForEach-Object {
+    Write-Host "Time: " $_.TimeCreated
+    Write-Host "Level: " $_.LevelDisplayName
+    Write-Host "Source: " $_.ProviderName
+    Write-Host "Log: " $_.LogName
+    Write-Host ""
+    Write-Host "Message: " $_.Message
+    Write-Host "---------------------------------------------------"
+    Write-Host ""
+}
